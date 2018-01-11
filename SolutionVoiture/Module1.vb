2b6eb22c5938e6cecb10465b2838cdf01f4024ca
Laurent FREVAL <lfreval@la-joliverie.com>
2018-01-11 11:54:38 +0100

    diapo 67

--- SolutionVoiture/Module1.vb
+++ SolutionVoiture/Module1.vb
@@ -3,11 +3,13 @@ Option Strict On
 
 Module Module1
     Dim maClio As New Voiture
+    Dim monTrajet As New Trajet
 
     Sub Main()
         System.Console.WriteLine(maClio)
         System.Console.WriteLine(maClio.leMoteur)
         System.Console.WriteLine(maClio.GetMoteur)
+        monTrajet.Saisir()
         System.Console.ReadKey()
     End Sub
 
--- SolutionVoiture/Trajet.vb
+++ SolutionVoiture/Trajet.vb
@@ -2,4 +2,11 @@
     Private _depart As String
     Private _arrivee As String
     Private _distance As Distance
+
+    Public Sub Saisir()
+        Console.Write("saisir Départ : ")
+        _depart = Console.ReadLine
+        Console.Write("saisir Arrivée : ")
+        _arrivee = Console.ReadLine
+    End Sub
 End Class
