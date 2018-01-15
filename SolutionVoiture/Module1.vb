7fbba455921e6148af4d4f99d0bffb57327cb5df
Laurent FREVAL <lfreval@la-joliverie.com>
2018-01-15 11:12:41 +0100

    diapo 106

--- SolutionVoiture/Module1.vb
+++ SolutionVoiture/Module1.vb
@@ -8,27 +8,27 @@ Module Module1
     Dim maClio As New Voiture
     Dim monTrajet As New Trajet("Nantes", "Bordeaux", 352)
 
-    Sub Help()
-        Console.WriteLine(My.Resources.Aide)
-
-    End Sub
-
-    Function NeedHelp() As Boolean
-        For Each argument As String In My.Application.CommandLineArgs
-            Select Case argument
-                Case "--help", "-help", "-h", "/?"
-                    Return True
-            End Select
-        Next
-        Return False
-    End Function
-
     Sub Main()
         'System.Console.WriteLine(maClio)
         'System.Console.WriteLine(maClio.leMoteur)
         'System.Console.WriteLine(maClio.GetMoteur)
         'monTrajet.Saisir()
+        Console.WriteLine(TestSaisir())
+        Console.ReadKey()
+        Environment.Exit(100)
+
+
+        Dim ville1 As String = CStr(Saisir("ville départ", "String"))
+        Dim ville2 As String = CType(Saisir("ville arrivée", "String"), String)
+        Dim distV1V2 As Integer = CInt(Saisir("distance", "Integer"))
+        Dim monTrajet2 As New Trajet(ville1, ville2, distV1V2)
+
+        Dim monTrajet3 As New Trajet(CStr(Saisir("ville départ", "String")), CType(Saisir("ville arrivée", "String"), String), CInt(Saisir("distance", "Integer")))
+
         monTrajet.Afficher()
+        monTrajet2.Afficher()
+        monTrajet3.Afficher()
+
 
         If NeedHelp() Then
             Help()
---
+++ SolutionVoiture/Outils.vb
@@ -0,0 +1,55 @@
+﻿Module Outils
+    Sub Help()
+        Console.WriteLine(My.Resources.Aide)
+
+    End Sub
+
+    Function NeedHelp() As Boolean
+        For Each argument As String In My.Application.CommandLineArgs
+            Select Case argument
+                Case "--help", "-help", "-h", "/?"
+                    Return True
+            End Select
+        Next
+        Return False
+    End Function
+
+    Function Saisir(nom As String, type As String) As Object
+        Console.Write("Saisie " & nom & ": ")
+        Dim valeur As String = Console.ReadLine
+        If type.ToUpper.Equals("STRING") Then
+            Return valeur
+        ElseIf UCase(type).Equals("INTEGER") And IsNumeric(valeur) Then
+            Return CInt(valeur)
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    Function TestSaisir() As Boolean
+        Dim OK As Boolean = True
+        Dim valeur As Object
+
+        valeur = Saisir("chaîne", "String")
+        If TypeOf valeur Is String Then
+            OK = OK And True
+        Else
+            OK = OK And False
+        End If
+        valeur = Saisir("entier", "Integer")
+        If TypeOf valeur Is Integer Then
+            OK = OK And True
+        Else
+            OK = OK And False
+        End If
+        valeur = Saisir("truc", "Autre Chose")
+        If valeur Is Nothing Then
+            OK = OK And True
+        Else
+            OK = OK And False
+        End If
+
+        Return OK
+    End Function
+
+End Module
